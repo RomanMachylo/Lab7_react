8552df376b845f91321435516e75f4fdc710eefe
RomanMachylo <maciloroman@gmail.com>
2022-11-29 14:47:04 +0200

    Added lab7

---
+++ src/Components/GoodCards/data.js
@@ -0,0 +1,30 @@
+export const DATA = [{
+    img: 'images/image1.jpg',
+    name: 'Електрочайник',
+    price: '1000',
+},
+{
+    img: 'images/image2.jpg',
+    name: 'Тостер',
+    price: '1300',
+},
+{
+    img: 'images/image3.jpg',
+    name: 'Холодильник',
+    price: '20000',
+},
+{
+    img: 'images/image4.jpg',
+    name: 'Мікрохвильова піч',
+    price: '17000',
+},
+{
+    img: 'images/image5.jpg',
+    name: 'Блендер',
+    price: '1800',
+},
+{
+    img: 'images/image6.jpg',
+    name: 'кавомолка',
+    price: '1100',
+}];
